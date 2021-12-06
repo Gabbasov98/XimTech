aec14fe95dad995bdba4a7072391e29e36cea5d5
Marsel Gabbasov <marsel13_98@mail.ru>
2021-12-06 21:21:36 +0300

    new pages

--- js/index.js
+++ js/index.js
@@ -108,18 +108,143 @@ function sliderService() {
     })
 }
 
+function sliderCatalog() {
+    var swiper = new Swiper('#catalog-slider1 .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '#catalog-slider1 .swiper-button-next',
+            prevEl: '#catalog-slider1 .swiper-button-prev',
+        },
+        breakpoints: {
+            280: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            400: {
+                slidesPerView: 1.5,
+                spaceBetween: 15
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            790: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            992: {
+                spaceBetween: 30,
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        }
+    })
+}
+
+function sliderCatalog2() {
+    var swiper = new Swiper('#catalog-slider2 .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '#catalog-slider2 .swiper-button-next',
+            prevEl: '#catalog-slider2 .swiper-button-prev',
+        },
+        breakpoints: {
+            280: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            400: {
+                slidesPerView: 1.5,
+                spaceBetween: 15
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            790: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            992: {
+                spaceBetween: 30,
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        }
+    })
+}
+
+function sliderCatalog3() {
+    var swiper = new Swiper('#catalog-slider3 .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '#catalog-slider3 .swiper-button-next',
+            prevEl: '#catalog-slider3 .swiper-button-prev',
+        },
+        breakpoints: {
+            280: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            400: {
+                slidesPerView: 1.5,
+                spaceBetween: 15
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            790: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            992: {
+                spaceBetween: 30,
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        }
+    })
+}
+
+function gallery() {
+    var swiper = new Swiper(".catalog-item .mySwiper", {
+        spaceBetween: 35,
+        slidesPerView: 'auto',
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".catalog-item .mySwiper2", {
+        spaceBetween: 10,
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+}
 
 $(document).ready(function() {
     sliderMain()
     sliderDiscount()
     sliderHit()
     sliderService()
+    sliderCatalog()
+    sliderCatalog2()
+    sliderCatalog3()
+    gallery()
+    cartCalc()
 
     $('.customScroll').mCustomScrollbar({
         axis: "x",
     });
 
     $(".custom-select").niceSelect()
+    $(".city-select").niceSelect()
 
     $('input[type="tel"]').mask('+7 (999) 999-9999', { placeholder: '+7 (   )    -    ' });
 
@@ -189,12 +314,84 @@ $(document).ready(function() {
 
     })
 
+    $(".catalog__sidebar-all").click(function() {
+        $(this).siblings(".catalog__sidebar-link").removeClass("catalog__sidebar-link--hidden")
+    })
+
+
+    $(".catalog__sidebar-title").click(function(e) {
+        e.preventDefault()
+        $(this).toggleClass("catalog__sidebar-title--active")
+    })
+
+    $(".catalog-nav__back").click(function(e) {
+        e.preventDefault()
+        $(this).toggleClass("catalog-nav__back--active")
+    })
+
+    $(".catalog-filter__title").click(function(e) {
+        $(this).toggleClass("catalog-filter__title--active")
+    })
+
+    $(".catalog-filter__open").click(function() {
+        $(".catalog-filter").slideToggle()
+    })
+
+
+    const rangeSlider = document.getElementById("range-slider")
+    const rangeInput0 = document.getElementById("input-range0")
+    const rangeInput1 = document.getElementById("input-range1")
+    const rangeInputs = [rangeInput0, rangeInput1]
+    if (rangeSlider) {
+        noUiSlider.create(rangeSlider, {
+            start: [0, 999999],
+            connect: true,
+            step: 1,
+            range: {
+                'min': 0,
+                'max': 999999
+            }
+        });
+    }
+    if (rangeSlider) {
+        rangeSlider.noUiSlider.on('update', function(values, handle) {
+            rangeInputs[handle].value = Math.round(values[handle])
+        })
+        rangeInputs.forEach((el, index) => {
+            el.addEventListener('change', (e) => {
+                setRangeSlider(index, e.currentTarget.value)
+            })
+        })
+    }
+
+
+    const setRangeSlider = (i, value) => {
+        let array = [null, null];
+        array[i] = value;
+
+        rangeSlider.noUiSlider.set(array);
+    };
+
+
+    $(".catalog-filter__all").click(function() {
+        $(this).siblings(".catalog-filter__check").removeClass("catalog-filter__check--hidden")
+    })
+
+    $(".catalog-item__tab").click(function() {
+        let path = $(this).attr("data-tabs-path")
+        $(".catalog-item__tab").removeClass("catalog-item__tab--active")
+        $(`.catalog-item__tab[data-tabs-path="${path}"]`).addClass("catalog-item__tab--active")
+        $(".catalog-item__content").removeClass("catalog-item__content--active")
+        $(`.catalog-item__content[data-content-path="${path}"]`).addClass("catalog-item__content--active")
+
+    })
 })
 
 
 
 function cartCalc() {
     $('.ccalc .ccalc-minus').click(function() {
+        console.log(true)
         let a = $(this).closest('.ccalc').find('input').val();
         if (a > 1) {
             let b = +a - 1;
